8325ef36a7a961f3a045b4c5eaec78cfbef184d7
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-05 22:29:20 +0200

    Add Profile dropdown to the Navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import {
     Disclosure,
-    DisclosureButton
+    DisclosureButton,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
 } from "@headlessui/react";
 import {
     Bars3Icon,
@@ -110,6 +114,63 @@ const Navbar: React.FC = () => {
                             </div>
 
                         </div>
+
+                        {/* Profile dropdown */}
+                        <Menu as="div" className="relative ml-4">
+                            <div>
+                                <MenuButton className="relative flex rounded-full bg-[#F3E5F5] text-sm focus:outline-none">
+                                    <div className="relative p-[2px] rounded-full border-rotate-wrapper">
+                                        <div className="border-[1.5px] border-dashed border-[#1140A6] rounded-full border-rotate h-12 w-12"></div>
+                                            <img
+                                                className="avatar-image h-10 w-10 rounded-full"
+                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                alt=""
+                                            />
+                                    </div>
+                                </MenuButton>
+                            </div>
+                            <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <MenuItem>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? "bg-gray-100" : "",
+                                                "block px-4 py-2 text-sm text-black"
+                                            )}
+                                        >
+                                            Dashboard
+                                        </a>
+                                    )}
+                                </MenuItem>
+                                <MenuItem>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? "bg-gray-100" : "",
+                                                "block px-4 py-2 text-sm text-black"
+                                            )}
+                                        >
+                                            Settings
+                                        </a>
+                                    )}
+                                </MenuItem>
+                                <MenuItem>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(
+                                                active ? "bg-gray-100" : "",
+                                                "block px-4 py-2 text-sm text-black"
+                                            )}
+                                        >
+                                            Log out
+                                        </a>
+                                    )}
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
                     </div>
                 </div>
             </div>            
