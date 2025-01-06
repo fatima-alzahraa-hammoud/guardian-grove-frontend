18e8c46075ab4f2b7975a06f90c3e0ca90f3d6fc
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-06 15:16:55 +0200

    Add Landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandinPageNavbar from "../components/common/LandingPageNavbar";
+
+const LandingPage : React.FC = () => {
+
+    return(
+        <div>
+            <LandinPageNavbar />
+        </div>
+    );
+}
+export default LandingPage;
