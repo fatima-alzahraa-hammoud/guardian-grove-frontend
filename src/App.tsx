02581e966a51d80d9d127747ae56e879cc14bbb0
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-06 15:15:27 +0200

    Add decoding token and fetching user code to MainDasboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,11 @@
 import './styles/App.css';
 import Login from './pages/Login';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import LandinPageNavbar from './components/common/LandingPageNavbar';
 import Main from './pages/MainDashboard';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { jwtDecode } from 'jwt-decode';
-import { toast, ToastContainer } from 'react-toastify';
-import { requestApi } from './libs/requestApi';
-import { requestMethods } from './libs/enum/requestMethods';
-import { setUser } from './redux/slices/userSlice';
-
-interface DecodedToken {
-  userId: string;
-  role: string;
-}
 
 function App() {
-
-  const dispatch = useDispatch();
-  const [userId, setUserId] = useState<string | null>(null);
-
-  const navigate = useNavigate();
-
-  const token = localStorage.getItem("token");
-      
-  useEffect(() =>{
-      if (token){
-          const decoded = jwtDecode<DecodedToken>(token);
-          console.log("Decoded token:", decoded); 
-          setUserId(decoded.userId);
-      }
-      else{
-          toast.error("You are not allowed to access the page");
-          navigate("/");
-      }
-  }, []);
-
-  useEffect(() =>{
-      const fetchUsers = async() =>{
-          try{
-
-              const response = await requestApi({
-                  route: "/users/user",
-                  method: requestMethods.GET,
-                  body: userId,
-              });
-
-              if (response){
-                  dispatch(setUser(response.user));
-              }else {
-                  toast.error(response.message || 'Getting user failed!');
-              }
-  
-          }catch(error){
-              toast.error('An error occurred during getting user.');
-          }
-      }
-
-      fetchUsers();
-  }, [userId]);
 
   return (
     <>
