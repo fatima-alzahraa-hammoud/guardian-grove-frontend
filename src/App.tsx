6143695f421cbeefd9aa87d14f6bfd597beac4fa
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-06 14:44:00 +0200

    Add decode token code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import './styles/App.css';
 import Login from './pages/Login';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Signup from './pages/Signup';
 import LandinPageNavbar from './components/common/LandingPageNavbar';
 import Main from './pages/MainDashboard';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { jwtDecode } from 'jwt-decode';
+import { toast, ToastContainer } from 'react-toastify';
+interface DecodedToken {
+  userId: string;
+  role: string;
+}
 
 function App() {
+
+  const [userId, setUserId] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token");
+      
+  useEffect(() =>{
+      if (token){
+          const decoded = jwtDecode<DecodedToken>(token);
+          console.log("Decoded token:", decoded); 
+          setUserId(decoded.userId);
+      }
+      else{
+          toast.error("You are not allowed to access the page");
+          navigate("/");
+      }
+  }, []);
 
   return (
     <>
       <div className='App'>
+        <ToastContainer />
         <Routes>
           <Route path='/' element={<Login />}/>
           <Route path='/signup' element={<Signup />}/>
