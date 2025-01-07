f2a4818c97232384371f0d64a3f69e6b174d18b3
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-08 00:52:13 +0200

    Add & Style locked Achievement content in AchievementCard

--- src/components/cards/AchievementCard.tsx
+++ src/components/cards/AchievementCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import coinImage from "../../assets/images/coins.png";
+import starsImage from "../../assets/images/stars.png";
 
 interface AchievementCardProps {
     title: string;
@@ -13,8 +15,8 @@ interface AchievementCardProps {
 
 const AchievementCard: React.FC<AchievementCardProps> = ({ title, description, photo, criteria, starsReward, coinsReward, isLocked, unlockedAt, }) => {
     return (
-        <div className="h-[200px] border border-[#3A8EBA] rounded-lg hover:shadow-md transition-all duration-300 font-poppins relative overflow-hidden cursor-pointer">
-            <div className="p-6 flex flex-col items-center justify-around gap-4">
+        <div className="h-[180px] border border-[#3A8EBA] justify-center rounded-lg hover:shadow-md transition-all duration-300 font-poppins relative overflow-hidden cursor-pointer">
+            <div className="p-6 flex flex-col items-center justify-center gap-4">
                 {/* Achievement Image */}
                 <div className="w-20 h-20 flex justify-center items-center bg-white rounded-full">
                     <img
@@ -43,6 +45,31 @@ const AchievementCard: React.FC<AchievementCardProps> = ({ title, description, p
                         </>
                     ) : (
                         <>
+                            <p className="text-sm font-medium">{criteria}</p>
+                            {(starsReward > 0 || coinsReward > 0) && (
+                                <div className="flex gap-7 justify-center items-center text-xs mt-4">
+                                    {starsReward > 0 && (
+                                        <span className="flex items-center gap-2 text-md">
+                                            <img
+                                                src={starsImage}
+                                                alt="Stars"
+                                                className="w-5 h-5"
+                                            />
+                                            {starsReward}
+                                        </span>
+                                    )}
+                                    {coinsReward > 0 && (
+                                        <span className="flex items-center gap-2 text-md">
+                                            <img
+                                                src={coinImage}
+                                                alt="Coins"
+                                                className="w-5 h-5"
+                                            />
+                                            {coinsReward}
+                                        </span>
+                                    )}
+                                </div>
+                            )}
                         </>
                     )}
                 </div>
