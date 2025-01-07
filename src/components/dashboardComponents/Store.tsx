e760a8a9d338c1f35f4b9ee83b81e3a71e3bb3d5
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-07 03:00:28 +0200

    Add Store component

---
+++ src/components/dashboardComponents/Store.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const store: React.FC = () => {
+
+    return(
+        <div className="h-screen">
+            <div>
+                {/*Header*/}
+                <div>
+                    <div>
+                        <h2>Welcome to your Magic Store</h2>
+                        <p>Spend your hard-earned coins to customize your Magic Garden and unlock rewards!</p>
+                    </div>
+                    <div></div>
+                </div>
+
+                {/*Filters*/}
+                <div>
+
+                </div>
+
+                {/* Item cards*/}
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default store;
