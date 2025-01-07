1116c750e46ac49322027f44c008514dd4824907
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-07 03:28:00 +0200

    Add Navbar to the store and Style

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,27 +1,32 @@
 import React from "react";
+import Navbar from "../components/dashboardComponents/NavBar";
 
 const Store: React.FC = () => {
-
-    return(
-        <div className="h-screen">
-            <div>
-                {/*Header*/}
-                <div>
-                    <div>
-                        <h2>Welcome to your Magic Store</h2>
-                        <p>Spend your hard-earned coins to customize your Magic Garden and unlock rewards!</p>
+    return (
+        <div className="h-screen flex flex-col">
+            {/* Navbar */}
+            <Navbar />
+            
+            {/* Main Content */}
+            <div className="flex-grow pt-20 px-6 font-poppins">
+                <div className="max-w-5xl mx-auto">
+                    {/* Header */}
+                    <div className="mb-10 text-left mt-5">
+                        <h2 className="text-2xl font-bold font-comic">Welcome to your Magic Store</h2>
+                        <p className="text-gray-600 mt-2 text-base">
+                            Spend your hard-earned coins to customize your Magic Garden and unlock rewards!
+                        </p>
                     </div>
-                    <div></div>
-                </div>
 
-                {/*Filters*/}
-                <div>
+                    {/* Filters Section */}
+                    <div className="mb-8">
+                        {/* Add filter buttons or dropdowns here */}
+                    </div>
 
-                </div>
-
-                {/* Item cards*/}
-                <div>
-
+                    {/* Item Cards Section */}
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {/* Item cards will be mapped here */}
+                    </div>
                 </div>
             </div>
         </div>
