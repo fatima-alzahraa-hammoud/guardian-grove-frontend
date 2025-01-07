52b009281372a9867d557d69497dc396e232a529
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-07 13:49:25 +0200

    Style StoreItem component

--- src/components/storeComponents/StoreItem.tsx
+++ src/components/storeComponents/StoreItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, CardContent } from "../ui/card";
-import { Button } from "../ui/button";
+import { cn } from "../../lib/utils";
+import coinImage from "../../assets/images/coin.png";
 
 interface StoreItemProps {
     name: string
@@ -8,27 +8,50 @@ interface StoreItemProps {
     image: string
     purchased?: boolean
 }
-  
-const storeItem : React.FC<StoreItemProps> = ({name, price, image, purchased}) => {
-    return(
-        <Card>
-            <CardContent>
-                <div>
-                    <img src={image} alt="" />
+
+const StoreItem: React.FC<StoreItemProps> = ({ name, price, image, purchased }) => {
+    return (
+        <div className="border border-[#3A8EBA] rounded-md hover:shadow-md transition font-poppins">
+            <div className="p-5 flex flex-col items-center gap-4">
+                {/* Item Image */}
+                <div className="w-24 h-24 flex justify-center items-center">
+                    <img 
+                        src={image}
+                        alt={name}
+                        className="w-20 h-20 object-contain"
+                    />
                 </div>
-                <h4>{name}</h4>
-                <div>
-                    <img src="" alt="" />
-                    <span>{price}</span>
+
+                {/* Item Name */}
+                <h4 className="text-lg font-medium text-center">{name}</h4>
+
+                {/* Price Section */}
+                <div className="flex items-center gap-4 text-gray-700">
+                    <img 
+                        src={coinImage}
+                        alt="Coins"
+                        width={20}
+                        height={20}
+                        className="w-5 h-5"
+                    />
+                    <span className="text-md">{price}</span>
                 </div>
-                <Button
+
+                {/* Buy Now Button */}
+                <button
+                    className={cn(
+                        "w-3/4 rounded-full font-semibold py-2 text-sm",
+                        purchased
+                            ? "bg-[#A6A6A6] hover:bg-[#A6A6A6] text-black cursor-not-allowed"
+                            : "bg-[#179447] hover:bg-green-600 text-white hover:text-white"
+                    )}
                     disabled={purchased}
                 >
                     {purchased ? "Purchased" : "Buy Now"}
-                </Button>
-            </CardContent>
-        </Card>
+                </button>
+            </div>
+        </div>
     );
 };
 
-export default storeItem;
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,14 +3,17 @@ import Navbar from "../components/dashboardComponents/NavBar";
 import coinIcon from "../assets/images/coins.png";
 import { Button } from "../components/ui/button";
 import { cn } from "../lib/utils";
+import StoreItem from "../components/storeComponents/StoreItem";
+import picture1 from "../assets/images/avatars/parent/avatar1.png";
+import picture2 from "../assets/images/avatars/parent/avatar2.png";
+import picture3 from "../assets/images/avatars/parent/avatar3.png";
+import picture4 from "../assets/images/avatars/parent/avatar4.png";
 
 const Store: React.FC = () => {
 
     const [activeFilter, setActiveFilter] = useState<string>("All");
 
     const filters = ["All", "Garden Items", "Pets", "Themes", "Gifts", "Games"];
-
-
 
     return (
         <div className="h-screen flex flex-col">
@@ -54,8 +57,17 @@ const Store: React.FC = () => {
                     </div>
 
                     {/* Item Cards Section */}
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {/* Item cards will be mapped here */}
+                    <div className="grid grid-cols-1 md:grid-cols-5 gap-6 mt-5">
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={picture1} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={picture2} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={picture3} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={picture4} name="coinn" price={10} purchased={true}/>
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={true}/>
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={false}/>
+                        <StoreItem image={coinIcon} name="coinn" price={10} purchased={false}/>
                     </div>
                 </div>
             </div>
