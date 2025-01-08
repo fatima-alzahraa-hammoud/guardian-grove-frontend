7b4df759296b34e3af03be8be8c67299d3216949
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-08 02:14:13 +0200

    Add dropdown menu to sortBy button in Achievements

--- src/components/dashboardComponents/Achievements.tsx
+++ src/components/dashboardComponents/Achievements.tsx
@@ -7,6 +7,7 @@ import notesImage from '/assets/images/dashboard/notes.png';
 import { requestApi } from "../../libs/requestApi";
 import { requestMethods } from "../../libs/enum/requestMethods";
 import { toast } from "react-toastify";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 
 interface Achievement {
     _id: string;
@@ -87,10 +88,36 @@ const Achievements : React.FC = () => {
                         <h2 className="text-xl font-bold font-comic">Achievements</h2>
                     </div>
 
-                    <button className="flex items-center justify-between bg-[#3A8EBA] px-3 py-2 rounded-full hover:bg-[#347ea5] transition">
-                        <img src={sortImage} alt="Sort" className="w-4 h-4 mr-1"/>
-                        <span className="font-semibold text-sm ml-2 text-white">Sort</span>
-                    </button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                className="flex items-center bg-[#3A8EBA] px-3 py-2 rounded-full hover:bg-[#347ea5] transition"
+                            >
+                                <img src={sortImage} alt="Sort" className="w-4 h-4 mr-1" />
+                                <span className="text-sm font-semibold text-white">
+                                    Sort by 
+                                </span>
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent>
+                            <DropdownMenuItem >
+                                Stars
+                            </DropdownMenuItem>
+                            <DropdownMenuItem >
+                                Coins
+                            </DropdownMenuItem>
+                            <DropdownMenuItem >
+                                Title
+                            </DropdownMenuItem>
+                            <DropdownMenuItem >
+                                Date
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Sort Order: 
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
 
                 {/* Filters Section */}
