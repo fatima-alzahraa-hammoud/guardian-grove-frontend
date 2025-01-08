979b3130054da0fa1346d54e779afc9432c76c8e
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-08 16:58:25 +0200

    Add image to MyProfile

--- src/components/dashboardComponents/MyProfile.tsx
+++ src/components/dashboardComponents/MyProfile.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectName } from "../../redux/slices/userSlice";
+import { selectAvatar, selectName } from "../../redux/slices/userSlice";
+import uploadImage from "/assets/images/camera.svg";
 
 const MyProfile : React.FC = () => {
 
     const name = useSelector(selectName);
+    const avatar = useSelector(selectAvatar);
     const [currentDate, setCurrentDate] = useState<string>("");
 
     useEffect(() => {
@@ -28,15 +30,32 @@ const MyProfile : React.FC = () => {
             </div>
 
             {/* Info */}
-            <div>
-                <div>
-                    {/* Image */}
-                    <div></div>
+            <div className="flex space-x-6 pt-10">
+                <div className="flex bg-[#3A8EBA] p-6 rounded-lg w-3/5">
+
+                    <div className="flex flex-col pl-2 items-center">
+                        {/* Image */}
+                        <div className="relative">
+                            <img 
+                                className="w-20 h-20 bg-purple-100 rounded-full"
+                                src={avatar || "https://via.placeholder.com/150"}
+                                alt="User Avatar"
+                            />
+                            <span className="absolute bottom-2 right-0.5 transform translate-y-1/4 w-5 h-5 bg-white rounded-full flex items-center justify-center z-10">
+                                <img
+                                    src={uploadImage}
+                                    alt="Camera Icon"
+                                    className="w-4 h-4"
+                                />
+                            </span>       
+                        </div>
+                        <p className="pt-3 text-sm text-white">Child</p>
+                    </div>
+
                     
                     {/* Details */}
                     <div></div>
                 </div>
-                <div></div>
             </div>
             {/* Daily message */}
 
