1d0549cbbce8b5917faaaee52947f42d4a379b9b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-08 14:35:47 +0200

    Add formatted current date to MyProfile

--- src/components/dashboardComponents/MyProfile.tsx
+++ src/components/dashboardComponents/MyProfile.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectName } from "../../redux/slices/userSlice";
 
 const MyProfile : React.FC = () => {
 
     const name = useSelector(selectName);
+    const [currentDate, setCurrentDate] = useState<string>("");
+
+    useEffect(() => {
+        const today = new Date();
+        const formattedDate = today.toLocaleDateString(undefined, {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+        setCurrentDate(formattedDate);
+    }, []);
+
 
     return(
         <div className="pt-20 h-screen flex flex-col font-poppins max-w-5xl px-6 flex-grow">
             {/* Header */}
             <div className="text-left">
                 <h1 className="font-comic font-extrabold mb-2 text-lg">Hi, {name}</h1>
-                <p className="text-sm">date</p>
+                <p className="text-sm">{currentDate}</p>
             </div>
 
             {/* Info */}
