b12b958fac189ecfebb6e96c61a4df2e42b4df5f
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-08 14:33:30 +0200

    Add MyProfile header

--- src/components/dashboardComponents/MyProfile.tsx
+++ src/components/dashboardComponents/MyProfile.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectName } from "../../redux/slices/userSlice";
 
 const MyProfile : React.FC = () => {
+
+    const name = useSelector(selectName);
+
     return(
-        <div></div>
+        <div className="pt-20 h-screen flex flex-col font-poppins max-w-5xl px-6 flex-grow">
+            {/* Header */}
+            <div className="text-left">
+                <h1 className="font-comic font-extrabold mb-2 text-lg">Hi, {name}</h1>
+                <p className="text-sm">date</p>
+            </div>
+
+            {/* Info */}
+
+            {/* Daily message */}
+
+            {/*Personal progress */}
+
+            {/* Need help */}
+        </div>
     );
 };
 
