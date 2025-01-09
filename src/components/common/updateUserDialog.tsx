ca9b9ba092901aa58d7f4ef9151c8d53f9ef28eb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-09 18:17:48 +0200

    Add name input to updateUserDialog

--- src/components/common/updateUserDialog.tsx
+++ src/components/common/updateUserDialog.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '../ui/dialog';
 import AvatarSelector from '../AvatarSelector';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { TUpdate, updateSchema } from '../../libs/types/updateTypes';
 
 interface DialogProps {
     isOpen: boolean;
@@ -19,10 +24,18 @@ interface DialogProps {
 const DialogComponent: React.FC<DialogProps> = ({ isOpen, onClose, onConfirm, title, confirmText = "Confirm", cancelText = "Cancel", role, name, gender, birthday, email }) => {
 
     const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
-    const [editName, setName] = useState<string>(name);
-    const [editEmail, setEmail] = useState<string>(email);
-    const [editGender, setGender] = useState<string>(gender);
-    const [editBirthday, setBirthday] = useState<Date>(birthday);
+    const [editName, setName] = useState<string>('');
+    const [editEmail, setEmail] = useState<string>('');
+    const [editGender, setGender] = useState<string>('');
+    const [editBirthday, setBirthday] = useState<Date | null>();
+
+    const { 
+        register, 
+        handleSubmit,
+        formState: { errors }
+    } = useForm<TUpdate>({
+        resolver: zodResolver(updateSchema)
+    });
 
     return (
         <Dialog open={isOpen} onOpenChange={(open) => !open && onClose()}>
@@ -36,6 +49,7 @@ const DialogComponent: React.FC<DialogProps> = ({ isOpen, onClose, onConfirm, ti
                     <h4 className="mb-2 text-sm -mx-[30px]">Choose an Avatar</h4>
                     <div className="flex">
                         <AvatarSelector
+                            {...register("avatar")}
                             selectedAvatar={selectedAvatar} 
                             onAvatarClick={(src) => {
                                 setSelectedAvatar(src)
@@ -44,6 +58,30 @@ const DialogComponent: React.FC<DialogProps> = ({ isOpen, onClose, onConfirm, ti
                         />
                     </div>
                 </div>
+
+                <form  className="space-y-3 w-full">
+                    <div className="mx-3 relative">
+                        <Label htmlFor="name" className="block text-xs font-medium text-gray-700 text-left mb-1">
+                            Name
+                        </Label>
+                        <div className="relative">
+                            <Input
+                                {...register("name")}
+                                id="name"   
+                                type="text" 
+                                placeholder={name} 
+                                className="flex-1 h-9 bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-xs file:font-medium file:text-foreground disabled:cursor-not-allowed disabled:opacity-50 pl-8 mt-1 placeholder:text-[10px] placeholder:text-black rounded-md border-[#3A8EBA] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#3A8EBA] sm:text-xs md:text-xs lg:text-xs" 
+                            />
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round text-gray-500">
+                                    <circle cx="12" cy="8" r="5"/>
+                                    <path d="M20 21a8 8 0 0 0-16 0"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
             </DialogContent>
         </Dialog>
     );
