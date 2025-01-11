a88d31e9cc0d7ae21ddfafedea87d1af3cedeecb
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-11 19:38:03 +0200

    Add header section to Leaderboard

--- src/pages/dashboardPages/Leaderboard.tsx
+++ src/pages/dashboardPages/Leaderboard.tsx
@@ -2,7 +2,20 @@ import React from "react";
 
 const Leaderboard : React.FC = () => {
     return(
-        <div></div>
+        <div className="pt-28 h-screen flex flex-col mx-auto">
+            <div className="max-w-5xl px-6 flex-grow font-poppins">
+                {/* Header */}
+                <div className="flex items-center justify-between">
+                    <div className="text-left">
+                        <h2 className="text-2xl font-bold font-comic">Family Leaderboard</h2>
+                        <p className="text-gray-600 mt-2 text-base">
+                            Shine together! See how your family ranks among others.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     );
 };
 
