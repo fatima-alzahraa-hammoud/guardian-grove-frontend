4d6dfca62c5304aa46ea8bfd9b67e105cc3d99a5
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-11 19:22:29 +0200

    Add Leaderboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Store from "./dashboardPages/Store";
 import Main from "./dashboardPages/MainDashboard";
 import AIFriend from "./dashboardPages/AIFriend";
+import Leaderboard from "./dashboardPages/Leaderboard";
 
 const Dashboard : React.FC = () => {
 
@@ -14,6 +15,7 @@ const Dashboard : React.FC = () => {
                 <Route path="/" element={<Main />}/>
                 <Route path="/store" element={<Store />}/>
                 <Route path="/AIFriend" element={<AIFriend />}/>
+                <Route path="/Leaderboard" element={<Leaderboard />}/>
             </Routes>
         </div>
     );
---
+++ src/pages/dashboardPages/Leaderboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Leaderboard : React.FC = () => {
+    return(
+        <div></div>
+    );
+};
+
+export default Leaderboard;
