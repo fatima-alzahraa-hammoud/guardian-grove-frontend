fa3bb5fe18b7abef12213875680fb7068c4decbe
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-12 21:18:32 +0200

    Add Question section to AddMembers page

--- src/pages/AddMembersPage.tsx
+++ src/pages/AddMembersPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import logo from '/assets/logo/GuardianGrove_logo_Text.png';
 
-const AddMembers :React.FC = () => {
+const AddMembersQuestion :React.FC = () => {
     return(
         <div className="min-h-screen bg-[#F5F1FA] flex flex-col items-center justify-center p-4 relative overflow-hidden">
             {/* Logo */}
@@ -14,8 +14,20 @@ const AddMembers :React.FC = () => {
                     height={120}
                 />
             </div>
+
+            {/* Question */}
+            <div className="max-w-2xl w-full backdrop-blur-sm p-6 space-y-6 rounded-2xl">
+                <div className="space-y-4 text-center">
+                    <h1 className="text-xl md:text-2xl font-bold font-comic">
+                        Would you like to create your family now?
+                    </h1>
+                    <p className="text-gray-600 font-poppins">
+                        Adding your family will unlock tailored experiences and features just for them.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 };
 
-export default AddMembers;
+export default AddMembersQuestion;
