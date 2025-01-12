72ff7d96bbd7058232172adcf2c55d0d68c9a7fe
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-12 23:58:59 +0200

    Add react hook form to AddMembersForm

--- src/components/dashboardComponents/AddMembersForm.tsx
+++ src/components/dashboardComponents/AddMembersForm.tsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
-import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import AvatarSelector from "../AvatarSelector";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addMemberSchema, TAddMember } from "../../libs/types/addMemberTypes";
 
 const AddMembersForm : React.FC = () => {
 
     const [tab, setTab] = useState<string>("child");
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: { errors },
+        reset
+    } = useForm<TAddMember>({
+        resolver: zodResolver(addMemberSchema),
+    });
 
     return(
         <div className="max-w-2xl w-full backdrop-blur-sm p-6 space-y-6 rounded-2xl">
@@ -19,6 +34,16 @@ const AddMembersForm : React.FC = () => {
                         <TabsTrigger value="parent" className="shrink-0" onClick={() => setTab("parent")}>Parent</TabsTrigger>
                     </TabsList>
                 </div>
+
+                <TabsContent value={tab}>
+                    {/* Avatar Selection */}
+                    <div className="mt-10">
+                        <h4 className="mb-2 text-xs font-medium -mx-[30px]">Change your avatar</h4>
+                        <div className="flex">
+                            
+                        </div>
+                    </div>
+                </TabsContent>
             </Tabs>
         </div>
     );
