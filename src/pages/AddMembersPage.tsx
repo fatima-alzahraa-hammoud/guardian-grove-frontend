761b5b106e5bbc894f225767611455d73eb92636
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-12 20:55:59 +0200

    Add logo to AddMembers page

--- src/pages/AddMembersPage.tsx
+++ src/pages/AddMembersPage.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import logo from '/assets/logo/GuardianGrove_logo_Text.png';
+
 const AddMembers :React.FC = () => {
     return(
-        <div>
-            
+        <div className="min-h-screen bg-[#F5F1FA] flex flex-col items-center justify-center p-4 relative overflow-hidden">
+            {/* Logo */}
+            <div className="absolute top-10 left-10">
+                <img
+                    src={logo}
+                    alt="Guardian Grove Logo"
+                    width={120}
+                    height={120}
+                />
+            </div>
         </div>
     )
 };
