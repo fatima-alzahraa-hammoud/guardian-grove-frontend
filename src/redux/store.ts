45b6ee89eb34e8c6fe5aa1f5d3e0df177351f546
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-12 03:34:34 +0200

    Add familyReducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import userReducer from './slices/userSlice';
+import familyReducer from './slices/familySlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    family: familyReducer,
   },
 });
 
