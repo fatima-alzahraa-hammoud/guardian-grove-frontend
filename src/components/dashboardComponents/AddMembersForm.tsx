e73e5d18c26112db9ac9e3e542c7db479ea06ed6
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-12 23:39:36 +0200

    Add header to AddMembersForm

--- src/components/dashboardComponents/AddMembersForm.tsx
+++ src/components/dashboardComponents/AddMembersForm.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const AddMembersForm : React.FC = () => {
     return(
         <div className="max-w-2xl w-full backdrop-blur-sm p-6 space-y-6 rounded-2xl">
-            
+            {/* Header */}
+            <h1 className="text-xl md:text-2xl font-bold font-comic">
+                Add Your Loved Ones to Begin the Adventure!
+            </h1>
         </div>
     );
 };
