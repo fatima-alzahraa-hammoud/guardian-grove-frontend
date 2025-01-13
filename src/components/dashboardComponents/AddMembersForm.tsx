52ea1ab7a3e2f5158c8520717d3ffd692a5c7c52
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-13 02:10:09 +0200

    Add birthday picker to AddMembersForm

--- src/components/dashboardComponents/AddMembersForm.tsx
+++ src/components/dashboardComponents/AddMembersForm.tsx
@@ -5,10 +5,16 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { addMemberSchema, TAddMember } from "../../libs/types/addMemberTypes";
 import { Input } from "../ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { cn } from "../../lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "../ui/calendar";
+import { format } from "date-fns";
 
 const AddMembersForm : React.FC = () => {
 
-    const [tab, setTab] = useState<string>("child");
+    const [tab, setTab] = useState<string>("Child");
 
     const {
         register,
@@ -21,6 +27,12 @@ const AddMembersForm : React.FC = () => {
         resolver: zodResolver(addMemberSchema),
     });
 
+    const selectedDate = watch("birthday");
+
+    const onDateSelect = (date: Date | undefined) => {
+        setValue("birthday", date || new Date("1900-01-01"), { shouldValidate: true });
+    };
+    
     return(
         <div className="max-w-2xl w-full backdrop-blur-sm p-6 space-y-6 rounded-2xl">
             {/* Header */}
@@ -28,11 +40,11 @@ const AddMembersForm : React.FC = () => {
                 Add Your Loved Ones to Begin the Adventure!
             </h1>
 
-            <Tabs defaultValue="child" className="w-full" value={tab} onValueChange={(value) => setTab(value)}>
+            <Tabs defaultValue="Child" className="w-full" value={tab} onValueChange={(value) => setTab(value)}>
                 <div className="flex justify-start items-center pl-20">
                     <TabsList className="flex flex-nowrap space-x-2 bg-[#CDE7FE]">
-                        <TabsTrigger value="child" className="shrink-0">Child</TabsTrigger>
-                        <TabsTrigger value="parent" className="shrink-0">Parent</TabsTrigger>
+                        <TabsTrigger value="Child" className="shrink-0">Child</TabsTrigger>
+                        <TabsTrigger value="Parent" className="shrink-0">Parent</TabsTrigger>
                     </TabsList>
                 </div>
 
@@ -51,7 +63,7 @@ const AddMembersForm : React.FC = () => {
 
                     <div className="w-full max-w-md">
                         <label htmlFor="username" className="block text-xs font-medium text-gray-700 text-left mb-1">
-                            Nicknane
+                            {tab}'s Nickname
                         </label>
                         <div className="relative">
                             <Input
@@ -59,7 +71,7 @@ const AddMembersForm : React.FC = () => {
                                 id="name" 
                                 type="text" 
                                 placeholder="nickname" 
-                                className="flex-1 h-9 bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-xs file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-xs pl-8 mt-1 placeholder:text-[10px] placeholder:text-gray-500 rounded-md border-[#3A8EBA] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#3A8EBA] max-w-md" 
+                                className="flex-1 h-9 bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-xs file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sn pl-8 mt-1 placeholder:text-[10px] placeholder:text-gray-500 rounded-md border-[#3A8EBA] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#3A8EBA] max-w-md" 
                             />
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-round text-gray-500">
@@ -68,6 +80,52 @@ const AddMembersForm : React.FC = () => {
                                 </svg>
                             </div>
                         </div>
+                    </div>
+
+                    {/* Birthday Picker */}
+                    <div className="w-full max-w-md">
+                        <div className="block text-xs font-medium text-gray-700 text-left mb-1">
+                            Date of Birth
+                        </div>
+                        <Popover>
+                            <PopoverTrigger asChild>
+                                <div>
+                                    <Button
+                                        variant={"outline"}
+                                        className={cn(
+                                            "w-full pl-3 pr-3 mt-1 placeholder:text-xs placeholder:text-gray-500 rounded-md border border-[#3A8EBA] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#3A8EBA] text-xs flex justify-between items-center font-normal font-poppins",
+                                            !selectedDate && "text-gray-500"
+                                        )}
+                                        style={{ backgroundColor: "transparent" }}
+                                        type="button"
+                                    >
+                                        {selectedDate ? (
+                                            format(selectedDate, "PPP")
+                                        ) : (
+                                            <span className="text-gray-500 font-normal text-xs">
+                                                Pick a date
+                                            </span>
+                                        )}
+                                        <CalendarIcon className="ml-auto w-2 h-2 opacity-60 text-gray-500" />
+                                    </Button>
+                                </div>
+                            </PopoverTrigger>
+                            <PopoverContent
+                                className="w-auto p-0 justify-center bg-white z-50 shadow-md rounded-md"
+                                style={{ pointerEvents: "auto" }} // Ensure it captures interaction
+                            >
+                                <Calendar
+                                    mode="single"
+                                    selected={selectedDate}
+                                    onSelect={onDateSelect}
+                                    disabled={(date) =>
+                                        date > new Date() || date < new Date("1900-01-01")
+                                    }
+                                    initialFocus
+                                    className="rounded-md justify-center items-center bg-white"
+                                />
+                            </PopoverContent>
+                        </Popover>
                     </div>
                 </TabsContent>
             </Tabs>
