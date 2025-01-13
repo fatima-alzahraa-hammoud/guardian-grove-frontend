00b2b53d89d362ebdbd68180cf9ba83613fb2230
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-13 23:26:24 +0200

    Add parents and children cards to FamilyTree

--- src/components/dashboardComponents/FamilyTree.tsx
+++ src/components/dashboardComponents/FamilyTree.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectFamilyMembers } from "../../redux/slices/familySlice";
+import FamilyMemberCard from "../cards/FamilyMemberCard";
+
 interface FamilyTreeProps {
     collapsed: boolean;
 }
+
+const parentColorPalette = [
+    "bg-blue-500",
+    "bg-purple-500",
+    "bg-teal-500",
+    "bg-rose-500",
+];
+
+const childColorPalette = [
+    "bg-green-400",
+    "bg-yellow-400",
+    "bg-red-400",
+    "bg-pink-400",
+    "bg-indigo-400",
+    "bg-orange-400",
+    "bg-cyan-400",
+];
+
   
 const FamilyTree : React.FC<FamilyTreeProps> = ({collapsed}) => {
+
+    const familyMembers = useSelector(selectFamilyMembers);
+
+    const parents = familyMembers.filter(member => member.role === "parent");
+    const children = familyMembers.filter(member => member.role === "child");
+
+
     return(
         <div className={`pt-24 min-h-screen flex flex-col items-center`}>
             <div className={`w-full flex-grow font-poppins ${ collapsed ? "mx-auto max-w-6xl" : "max-w-5xl" }`} >
@@ -16,6 +45,31 @@ const FamilyTree : React.FC<FamilyTreeProps> = ({collapsed}) => {
                     </p>
                 </div>
 
+                {/* Parents Row */}
+                <div className="mt-10 mb-14">
+                    <div className="flex justify-center space-x-20 mb-12">
+                        {parents.map((parent, index) => (
+                            <FamilyMemberCard
+                                key={parent.name}
+                                member={parent}
+                                colorClass={parentColorPalette[index % parentColorPalette.length]}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                {/* Children Row */}
+                <div>
+                    <div className="flex flex-wrap justify-center gap-x-10 gap-y-10">
+                        {children.map((child, index) => (
+                            <FamilyMemberCard
+                                key={child.name}
+                                member={child}
+                                colorClass={childColorPalette[index % childColorPalette.length]}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
