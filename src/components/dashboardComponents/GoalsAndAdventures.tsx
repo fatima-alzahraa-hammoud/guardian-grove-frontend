ef90b6ed06df3058ad95e31ebb25805e6bf34613
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 20:21:20 +0200

    Add Calendar to GoalsAndAdventures

--- src/components/dashboardComponents/GoalsAndAdventures.tsx
+++ src/components/dashboardComponents/GoalsAndAdventures.tsx
@@ -11,6 +11,9 @@ import { toast } from "react-toastify";
 import GoalCard from "../cards/GoalCard";
 import TasksDialog from "../common/TasksDialog";
 import Adventures from "./Adventures";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { Value } from "react-calendar/dist/esm/shared/types.js";
 
 interface FamilyTreeProps {
     collapsed: boolean;
@@ -49,6 +52,7 @@ const GoalsAndAdventures : React.FC<FamilyTreeProps> = ({collapsed}) => {
     const [goals, setGoals] = useState<Goal[]>([]);
     const [adventures, setAdventures] = useState<Adventure[]>([]);
     const [selectedAdventures, setSelectedAdventures] = useState<Adventure | null>(null);
+    const [selectedDate, setSelectedDate] = useState<Date>(new Date()); // Default to today's date
 
     const [selectedGoal, setSelectedGoal] = useState<Goal | null>(null);
     const [dialogOpen, setDialogOpen] = useState(false);
@@ -104,59 +108,80 @@ const GoalsAndAdventures : React.FC<FamilyTreeProps> = ({collapsed}) => {
         }
     };
 
+    const handleDateChange = (date: Value) => {
+        if (date instanceof Date) {
+            console.log("Selected Date:", date);
+            setSelectedDate(date); 
+        } else {
+            console.error("Invalid date selected:", date)
+        }
+    };
+
     return(
         <div className={`pt-24 min-h-screen flex flex-col items-center`}>
             <div className={`w-full flex-grow font-poppins ${ collapsed ? "mx-auto max-w-6xl" : "max-w-5xl" }`} >
-                
-                {/* Header */}
-                <div className="text-left">
-                    <h2 className="text-xl font-bold font-comic">
-                        {activeFilter === "Goals"?  "Conquer Goals, Embark on Adventures" : "Adventure Awaits!"}
-                    </h2>
-                    <p className="text-gray-600 mt-2 text-base">
-                        {activeFilter === "Goals"?  "Experience exciting adventures, accomplish meaningful goals, and rise to thrilling challenges together!" : "Take on exciting challenges and achieve your dreams together!"}
-                    </p>
-                </div>
-
-                {/* Filters and Buttons */}
-                <div className="flex items-center justify-between mt-10">
-                    {/* Filters Section */}
-                    <div className="flex flex-wrap gap-3">
-                        {filters.map((filter) => (
-                            <Button
-                                key={filter}
-                                onClick={() => setActiveFilter(filter)}
-                                variant="secondary"
-                                className={cn(
-                                    "bg-[#E3F2FD] hover:bg-[#d7edfd] w-36 text-black",
-                                    activeFilter === filter && "bg-[#3A8EBA] text-white hover:bg-[#347ea5]"
-                                )}
-                            >
-                                {filter}
-                            </Button>
-                        ))}
-                    </div>
-
-                    {activeFilter === "Goals" ? (
-                        /* Sort Button */
-                        <div className="flex items-center space-x-3">
-                            <Button className="flex items-center bg-[#F09C14] px-3 py-2 rounded-full hover:bg-[#EB9915] transition">
-                                <img src={sortImage} alt="Sort" className="w-4 h-4 mr-1" />
-                                <span className="text-sm font-semibold text-white">Sort</span>
-                            </Button>
-                            <Button className="flex items-center bg-[#179447] px-3 py-2 rounded-full hover:bg-[#158C43] transition">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus text-white">
-                                    <path d="M5 12h14"/><path d="M12 5v14"/>
-                                </svg>                            
-                                <span className="text-sm font-semibold text-white">Add Goal</span>
-                            </Button>
-                            <Button className="flex items-center bg-[#3A8EBA] px-3 py-2 rounded-full hover:bg-[#326E9F] transition">
-                                <img src={magicImage} alt="Sort" className="w-4 h-4 mr-1" />
-                                <span className="text-sm font-semibold text-white">Generate Goal</span>
-                            </Button>
+                <div className="flex justify-between w-full">
+                    <div className="w-full">
+                        {/* Header */}
+                        <div className="text-left">
+                            <h2 className="text-xl font-bold font-comic">
+                                {activeFilter === "Goals"?  "Conquer Goals, Embark on Adventures" : "Adventure Awaits!"}
+                            </h2>
+                            <p className="text-gray-600 mt-2 text-base">
+                                {activeFilter === "Goals"?  "Experience exciting adventures, accomplish meaningful goals, and rise to thrilling challenges together!" : "Take on exciting challenges and achieve your dreams together!"}
+                            </p>
                         </div>
-                    ) : (
-                        <div></div>
+
+                        {/* Filters and Buttons */}
+                        <div className="flex items-center justify-between mt-10">
+                            {/* Filters Section */}
+                            <div className="flex flex-wrap gap-3">
+                                {filters.map((filter) => (
+                                    <Button
+                                        key={filter}
+                                        onClick={() => setActiveFilter(filter)}
+                                        variant="secondary"
+                                        className={cn(
+                                            "bg-[#E3F2FD] hover:bg-[#d7edfd] w-36 text-black",
+                                            activeFilter === filter && "bg-[#3A8EBA] text-white hover:bg-[#347ea5]"
+                                        )}
+                                    >
+                                        {filter}
+                                    </Button>
+                                ))}
+                            </div>
+
+                            {activeFilter === "Goals" ? (
+                                /* Sort Button */
+                                <div className="flex items-center space-x-3">
+                                    <Button className="flex items-center bg-[#F09C14] px-3 py-2 rounded-full hover:bg-[#EB9915] transition">
+                                        <img src={sortImage} alt="Sort" className="w-4 h-4 mr-1" />
+                                        <span className="text-sm font-semibold text-white">Sort</span>
+                                    </Button>
+                                    <Button className="flex items-center bg-[#179447] px-3 py-2 rounded-full hover:bg-[#158C43] transition">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus text-white">
+                                            <path d="M5 12h14"/><path d="M12 5v14"/>
+                                        </svg>                            
+                                        <span className="text-sm font-semibold text-white">Add Goal</span>
+                                    </Button>
+                                    <Button className="flex items-center bg-[#3A8EBA] px-3 py-2 rounded-full hover:bg-[#326E9F] transition">
+                                        <img src={magicImage} alt="Sort" className="w-4 h-4 mr-1" />
+                                        <span className="text-sm font-semibold text-white">Generate Goal</span>
+                                    </Button>
+                                </div>
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>
+                    </div>
+                    {activeFilter === "Adventures" && (
+                        <div className="flex justify-center mb-6">
+                        <Calendar
+                            onChange={(date) => handleDateChange(date)}
+                            value={selectedDate}
+                            className=" shadow-lg rounded-lg bg-white p-4 border border-gray-200"
+                        />
+                        </div>
                     )}
                 </div>
 
