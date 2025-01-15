0aa700b95a1c9f8f4275f5a1b550936fab4a35f2
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 04:59:03 +0200

    Add & Style filters section in goals

--- src/components/dashboardComponents/Goals.tsx
+++ src/components/dashboardComponents/Goals.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { cn } from "../../lib/utils";
 
 
 interface FamilyTreeProps {
@@ -6,6 +8,10 @@ interface FamilyTreeProps {
 }
 
 const Goals : React.FC<FamilyTreeProps> = ({collapsed}) => {
+
+    const filters = ["Goals", "Adventures"];
+    const [activeFilter, setActiveFilter] = useState<string>("Goals");
+
     return(
         <div className={`pt-24 min-h-screen flex flex-col items-center`}>
             <div className={`w-full flex-grow font-poppins ${ collapsed ? "mx-auto max-w-6xl" : "max-w-5xl" }`} >
@@ -17,6 +23,23 @@ const Goals : React.FC<FamilyTreeProps> = ({collapsed}) => {
                         Experience exciting adventures, accomplish meaningful goals, and rise to thrilling challenges together!
                     </p>
                 </div>
+
+                {/* Filters Section */}
+                <div className="flex flex-wrap gap-3 mt-10">
+                    {filters.map((filter) => (
+                        <Button
+                            key={filter}
+                            onClick={() => setActiveFilter(filter)}
+                            variant="secondary"
+                            className={cn(
+                                "bg-[#E3F2FD] hover:bg-[#d7edfd] w-44 text-black",
+                                activeFilter === filter && "bg-[#3A8EBA] text-white hover:bg-[#347ea5]"
+                            )}
+                        >
+                            {filter}
+                    </Button>
+                ))}
+            </div>
             </div>
         </div>
     );
