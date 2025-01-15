425f2ba7a4e8f24214a69d5fbb07cdd422e194e2
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 23:14:27 +0200

    Add AINotifications component

---
+++ src/components/dashboardComponents/AINotifications.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface AINotificationsProps {
+    collapsed: boolean;
+}
+
+const AINotifications : React.FC <AINotificationsProps> = ({collapsed}) => {
+    return(
+        <div></div>
+    );
+};
+
+export default AINotifications;
