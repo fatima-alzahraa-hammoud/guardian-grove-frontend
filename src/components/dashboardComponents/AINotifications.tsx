52b198d1c8011ac7676c11e36e759d28204a4ea0
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 23:30:41 +0200

    Add filters section to AINotifications

--- src/components/dashboardComponents/AINotifications.tsx
+++ src/components/dashboardComponents/AINotifications.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectRole } from "../../redux/slices/userSlice";
+import { Button } from "../ui/button";
+import { cn } from "../../lib/utils";
 
 interface AINotificationsProps {
     collapsed: boolean;
 }
 
 const AINotifications : React.FC <AINotificationsProps> = ({collapsed}) => {
+
+    const role = useSelector(selectRole);
+    const filters = role === "parent" ?  ['Tips & Suggestions', 'Alerts & Notifications', 'Children’s Notify & Tips'] : ['Tips & Suggestions', 'Alerts & Notifications'];
+    const [activeFilter, setActiveFilter] = useState<string>("Tips & Suggestions");
+
     return(
         <div className={`pt-24 min-h-screen flex flex-col items-center`}>
             <div className={`w-full flex-grow font-poppins ${ collapsed ? "mx-auto max-w-6xl" : "max-w-5xl" }`} >
@@ -16,6 +25,23 @@ const AINotifications : React.FC <AINotificationsProps> = ({collapsed}) => {
                     <p className="text-gray-600 mt-2 text-base w-[75%]">
                         Your AI companion provides personalized tips and timely alerts to help you and your family stay safe, motivated, and connected.
                     </p>
+                </div>
+
+                {/* Filters Section */}
+                <div className="flex flex-wrap gap-3 mt-10">
+                    {filters.map((filter) => (
+                        <Button
+                            key={filter}
+                            onClick={() => setActiveFilter(filter)}
+                            variant="secondary"
+                            className={cn(
+                                "bg-[#E3F2FD] hover:bg-[#d7edfd] w-52 text-black",
+                                activeFilter === filter && "bg-[#3A8EBA] text-white hover:bg-[#347ea5]"
+                            )}
+                        >
+                            {filter}
+                        </Button>
+                    ))}
                 </div>
             </div>
         </div>
