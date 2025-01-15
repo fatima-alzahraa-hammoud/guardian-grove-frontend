31d16ae86629ee77a5af4c7345ab781c6375c979
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-16 00:31:29 +0200

    Add Notification interface to NotificationCard

--- src/components/cards/NotificationCard.tsx
+++ src/components/cards/NotificationCard.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+interface Notification {
+    title: string;
+    type: string;
+    description: string;
+    timestamp: Date;
+}
+
 const NotificationCard : React.FC = () => {
     return(
         <div></div>
