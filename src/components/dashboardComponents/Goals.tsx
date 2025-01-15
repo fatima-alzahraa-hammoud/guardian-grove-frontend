a2501fa7994992ac8c5e8b59ad3492177c0fbbd8
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 04:55:08 +0200

    Add & Style goals header

--- src/components/dashboardComponents/Goals.tsx
+++ src/components/dashboardComponents/Goals.tsx
@@ -7,7 +7,18 @@ interface FamilyTreeProps {
 
 const Goals : React.FC<FamilyTreeProps> = ({collapsed}) => {
     return(
-        <div></div>
+        <div className={`pt-24 min-h-screen flex flex-col items-center`}>
+            <div className={`w-full flex-grow font-poppins ${ collapsed ? "mx-auto max-w-6xl" : "max-w-5xl" }`} >
+                
+                {/* Header */}
+                <div className="text-left">
+                    <h2 className="text-xl font-bold font-comic">Conquer Goals, Embark on Adventures</h2>
+                    <p className="text-gray-600 mt-2 text-base">
+                        Experience exciting adventures, accomplish meaningful goals, and rise to thrilling challenges together!
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 };
 
