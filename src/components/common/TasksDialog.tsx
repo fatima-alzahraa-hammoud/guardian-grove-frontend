2eddfb5f56795454e9326bdb86bfa654d2b3982b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-15 16:27:18 +0200

    Add interfaces to TasksDialog

--- src/components/common/TasksDialog.tsx
+++ src/components/common/TasksDialog.tsx
@@ -1,6 +1,29 @@
 import React from "react";
 
-const TasksDialog : React.FC = () => {
+interface Task {
+    _id: string;
+    title: string;
+    completed: boolean;
+}
+
+interface Goal {
+    _id: string;
+    title: string;
+    tasks: Task[];
+    rewards: {
+        stars: number;
+        coins: number;
+        achievementName?: string;
+    };
+}
+
+interface TasksDialogProps {
+    goal: Goal | null;
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+const TasksDialog : React.FC<TasksDialogProps> = () => {
     return(
         <div></div>
     );
