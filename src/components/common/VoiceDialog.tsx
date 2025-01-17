34fb3932810e02fb83fde0399c54a2821922d8ae
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-17 21:44:35 +0200

    Add Dialog and DialogProps to VoiceDialog

--- src/components/common/VoiceDialog.tsx
+++ src/components/common/VoiceDialog.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Dialog } from "../ui/dialog";
 
-const VoiceDialog : React.FC = () => {
+
+interface VoiceDialogProps {
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+const VoiceDialog : React.FC<VoiceDialogProps> = ({open, onOpenChange}) => {
     return(
-        <div></div>
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            
+        </Dialog>
     );
 };
 
