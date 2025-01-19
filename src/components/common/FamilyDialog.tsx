3e8baedc7477f69794cda3a84ec3067c79c10a56
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-19 19:33:06 +0200

    Add winning section to FamilyDialog

--- src/components/common/FamilyDialog.tsx
+++ src/components/common/FamilyDialog.tsx
@@ -5,9 +5,11 @@ import { DialogContent, DialogTitle } from "@mui/material";
 interface FamilyDialogProps {
     familyName: string;
     rank: number;
+    totalStars: number;
+    wonChallenges: number;
 }
 
-const FamilyDialog : React.FC<FamilyDialogProps> = ({familyName, rank}) => {
+const FamilyDialog : React.FC<FamilyDialogProps> = ({familyName, rank, totalStars, wonChallenges}) => {
 
     return (
         <Dialog>
@@ -25,6 +27,22 @@ const FamilyDialog : React.FC<FamilyDialogProps> = ({familyName, rank}) => {
                         </div>
                     </DialogTitle>
                 </DialogHeader>
+
+                <div className="mt-2">
+                    {/* winning section */}
+                    <div className="mb-6 font-comic">
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-2">
+                                <span className="font-semibold">Total Stars:</span>
+                                <span>{totalStars}</span>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <span className="font-semibold">Challenges Won:</span>
+                                <span>{wonChallenges}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     );
