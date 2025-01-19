9850e220088cff266f660960aa88ef000ea51302
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-20 00:18:44 +0200

    Add QuickTipDialog component

---
+++ src/components/common/QuickTipDialog.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "../ui/dialog";
+import { Lightbulb } from "lucide-react";
+
+export interface QuickTipProps {
+    tip: {
+        title: string;
+        message: string;
+    }
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+const QuickTipDialog : React.FC<QuickTipProps> = ({ open, onOpenChange, tip }) => {
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle className="flex items-center gap-2 text-sky-800">
+                        <Lightbulb className="w-5 h-5 text-[#3A8EBA]" />
+                        {tip.title}
+                    </DialogTitle>
+                    <DialogDescription className="text-sky-800">
+                        {tip.message}
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
+export default QuickTipDialog;
