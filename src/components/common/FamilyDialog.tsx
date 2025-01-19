5da716c5958a6797867b98bd164e33565acdd1aa
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-19 19:30:02 +0200

    Add dailog header to FamilyDialog

--- src/components/common/FamilyDialog.tsx
+++ src/components/common/FamilyDialog.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { Dialog, DialogHeader } from "../ui/dialog";
+import { DialogContent, DialogTitle } from "@mui/material";
 
-const FamilyDialog : React.FC = () => {
+interface FamilyDialogProps {
+    familyName: string;
+    rank: number;
+}
+
+const FamilyDialog : React.FC<FamilyDialogProps> = ({familyName, rank}) => {
+
     return (
-        <div></div>
+        <Dialog>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        <div>
+                            <span className=" font-bold">Family Name: </span>
+                            <span className="font-semibold">{familyName}</span>
+                        </div>
+
+                        <div>
+                            <span className="font-semibold">Rank: </span>
+                            <span className="font-medium">{rank}</span>
+                        </div>
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     );
 };
 
