d165204ad12c7a58d4f5828fe050df4d1b65d75c
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-20 07:12:58 +0200

    Add ChallengeDialogProps interface

--- src/components/common/ChallengeDialog.tsx
+++ src/components/common/ChallengeDialog.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 
-const ChallengeDialog : React.FC = () => {
+
+interface ChallengeDialogProps {
+    isOpen: boolean;
+    onClose: () => void;
+    challenge: {
+      title: string;
+      content: string;
+      starsReward: number;
+      coinsReward: number;
+    } | null;
+    adventureTitle: string;
+    adventureStars: number;
+    adventureCoins: number;
+    challengeNumber: number;
+    totalChallenges: number;
+}
+
+const ChallengeDialog : React.FC<ChallengeDialogProps> = () => {
     return(
         <div></div>
     )
