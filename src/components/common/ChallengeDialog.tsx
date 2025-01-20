2f9ed5bef46d5fb57c65852f5a99e3966e28e912
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-01-20 07:23:29 +0200

    Add dialog header to ChallengeDialog

--- src/components/common/ChallengeDialog.tsx
+++ src/components/common/ChallengeDialog.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Coins, Star } from "lucide-react";
 
 
 interface ChallengeDialogProps {
@@ -17,10 +19,34 @@ interface ChallengeDialogProps {
     totalChallenges: number;
 }
 
-const ChallengeDialog : React.FC<ChallengeDialogProps> = () => {
-    return(
-        <div></div>
-    )
+const ChallengeDialog : React.FC<ChallengeDialogProps> = ({isOpen, onClose, challenge, adventureTitle, adventureStars, adventureCoins, totalChallenges, challengeNumber}) => {
+    if (!challenge) return null;
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onClose}>
+             <DialogContent className="max-w-lg p-10">
+                <DialogHeader>
+                    <DialogTitle className="flex flex-col justify-between font-comic text-lg">
+                        <div>
+                            <span className=" font-bold">Adventure: </span>
+                            <span className="font-semibold">{adventureTitle}</span>
+                        </div>
+
+                        <div className="flex items-center justify-between mt-5">
+                            <div>
+                                <span className="font-semibold">Total Stars: </span>
+                                <span className="font-medium">{challenge.starsReward}</span>
+                            </div>
+                            <div>
+                                <span className="font-semibold">Total Coins: </span>
+                                <span className="font-medium">{challenge.coinsReward}</span>
+                            </div>
+                        </div>
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
 };
 
 export default ChallengeDialog;
