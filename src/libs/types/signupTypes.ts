f2fe054f8ff1b589bf70308ca17dc3844abf25f3
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-05-17 14:16:35 +0300

    Fix login and signup types

--- src/libs/types/signupTypes.ts
+++ src/libs/types/signupTypes.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const firstStepSchema = z.object({
     name: z.string().min(3, { message: "Name must be at least 3 characters long" }),
-    email: z.string().regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, { message: "Invalid email format" }),
+    email: z.string().min(1, "Email is required").regex(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, { message: "Invalid email format" }),
     password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, { message: "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character" }),
     confirmPassword: z.string(),
 }).refine(data => data.password === data.confirmPassword, {
