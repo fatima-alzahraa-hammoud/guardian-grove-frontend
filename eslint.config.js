657a181e0f530788e46bdb64015f95b89a7804c7
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-20 12:47:39 +0300

    Fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,11 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
+
+  {
+    files: ['**/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 )
