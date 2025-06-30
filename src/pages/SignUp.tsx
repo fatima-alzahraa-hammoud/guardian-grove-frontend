52d0637d73a747056dfd3eed5ef0734586949538
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2025-06-30 09:28:17 +0300

    Update from let to const

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -89,7 +89,7 @@ const Signup : React.FC = () => {
 
             // Debug: Log FormData contents
             console.log('FormData contents:');
-            for (let [key, value] of formData.entries()) {
+            for (const [key, value] of formData.entries()) {
                 console.log(key, value);
             }
 
